c24693b632db6679ba11c23aa0a6da9435f39823
grzegorzxpatyk <grzegorzxpatyk@gmail.com>
2024-05-31 03:17:59 +0200

    change props in Navigation components

--- src/components/NavLinks.tsx
+++ src/components/NavLinks.tsx
@@ -1,30 +1,30 @@
 import { motion } from 'framer-motion';
 
 export default function NavLinks({
-  routes,
-  pathname,
+    routes,
+    pathname,
 }: {
-  routes: string[];
-  pathname: string;
+    routes: string[];
+    pathname: string;
 }) {
-  return (
-    <>
-      {routes.map((element) => (
-        <a key={element} href={`/${element}`} className='relative'>
-          {element === '' ? 'home' : element}
-          {pathname === `/${element}` && (
-            <motion.span
-              layoutId='navlink-underline'
-              transition={{
-                type: 'spring',
-                stiffness: 300,
-                damping: 15,
-              }}
-              className='absolute left-0 top-full block h-[2px] w-full bg-zinc-800 dark:bg-zinc-500'
-            />
-          )}
-        </a>
-      ))}
-    </>
-  );
+    return (
+        <>
+            {routes.map((element) => (
+                <a key={element} href={`/${element}`} className='relative'>
+                    {element === '' ? 'home' : element}
+                    {pathname === `/${element}` && (
+                        <motion.span
+                            layoutId='navlink-underline'
+                            transition={{
+                                type: 'spring',
+                                stiffness: 300,
+                                damping: 15,
+                            }}
+                            className='absolute left-0 top-full block h-[2px] w-full bg-zinc-800 dark:bg-zinc-500'
+                        />
+                    )}
+                </a>
+            ))}
+        </>
+    );
 }
